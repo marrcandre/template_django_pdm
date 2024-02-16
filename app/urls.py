1798ac59c2b8282e5e3e9d80ca4a9d7c28187296
Marco André Mendes <marcoandre@gmail.com>
2024-02-16 15:44:16 -0300

    Corrigir registro de rota para usuários

--- app/urls.py
+++ app/urls.py
@@ -13,7 +13,7 @@ from core.views import UserViewSet
 
 router = DefaultRouter()
 
-router.register("users", UserViewSet, basename="users")
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
