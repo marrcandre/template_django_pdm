14fa03df5b73c6ab10183aaddef1b5800547c9b8
Marco André Mendes <marcoandre@gmail.com>
2024-05-16 09:52:24 -0300

    Mudança no print das configurações

--- app/settings.py
+++ app/settings.py
@@ -108,6 +108,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "static/"
 
+# App Uploader settings
+MEDIA_ENDPOINT = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+FILE_UPLOAD_PERMISSIONS = 0o640
+
 if MODE == "DEVELOPMENT":
     MY_IP = os.getenv("MY_IP", "127.0.0.1")
     MEDIA_URL = f"http://{MY_IP}:19003/media/"
@@ -138,4 +143,4 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
 }
 
-print(f"MODE: {MODE} \nMEDIA_URL: {MEDIA_URL} \nDATABASE: {DATABASES}")
+print(f"{MODE = } \n{MEDIA_URL = } \n{DATABASES = }")
