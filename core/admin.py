20394e2d4efb7ea8647d6623cc5169ede3b7475c
Marco André Mendes <marcoandre@gmail.com>
2024-07-09 13:27:38 -0300

    Inclusão da seção de grupos no admin.py

--- core/admin.py
+++ core/admin.py
@@ -28,6 +28,7 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
         (_("Important dates"), {"fields": ("last_login",)}),
+        (_("Groups"), {"fields": ("groups",)}),
     )
     readonly_fields = ["last_login"]
     add_fieldsets = (
