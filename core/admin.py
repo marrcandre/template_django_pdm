8781964d53d191b18a0bae6b7d597920ee975cae
Marco André Mendes <marcoandre@gmail.com>
2024-07-22 14:47:26 -0300

    Adicionar permissões de usuários ao Admin do Django,

--- core/admin.py
+++ core/admin.py
@@ -29,6 +29,7 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login",)}),
         (_("Groups"), {"fields": ("groups",)}),
+        (_("User Permissions"), {"fields": ("user_permissions",)}),
     )
     readonly_fields = ["last_login"]
     add_fieldsets = (
